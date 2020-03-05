b756d2f60286b67b502c6515097775cdc66de4cc
Ruben <fceruben@gmail.com>
2020-03-05 10:51:26 +0000

    limpeza de codigo

--- src/main/java/pt/apollotec/widoes/MainActivity.java
+++ src/main/java/pt/apollotec/widoes/MainActivity.java
@@ -1,7 +1,5 @@
 package pt.apollotec.widoes;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.annotation.SuppressLint;
 import android.content.res.Configuration;
 import android.os.Build;
@@ -12,6 +10,9 @@ import android.view.WindowManager;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 import pt.apollotec.R;
 
@@ -42,7 +43,9 @@ public class MainActivity extends AppCompatActivity {
         Mywebview.setHorizontalScrollBarEnabled(false);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
     }
-    void HideNav(){
+
+    @SuppressLint("InlinedApi")
+    void HideNav() {
         this.getWindow().getDecorView()
                 .setSystemUiVisibility(
                         View.SYSTEM_UI_FLAG_FULLSCREEN |
@@ -52,7 +55,7 @@ public class MainActivity extends AppCompatActivity {
                                 View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
                                 View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                 );
-        if( Build.VERSION.SDK_INT < 19) { // lower api
+        if (Build.VERSION.SDK_INT < 19) { // lower api
             View v = this.getWindow().getDecorView();
             v.setSystemUiVisibility(View.GONE);
         } else {
@@ -62,6 +65,7 @@ public class MainActivity extends AppCompatActivity {
             decorView.setSystemUiVisibility(uiOptions);
         }
     }
+
     @Override
     public boolean onTouchEvent(MotionEvent event) {
         HideNav();
@@ -69,7 +73,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    public void onConfigurationChanged(Configuration newConfig) {
+    public void onConfigurationChanged(@NonNull Configuration newConfig) {
         super.onConfigurationChanged(newConfig);
         HideNav();
     }
--- src/main/java/pt/apollotec/widoes/PinActivity.java
+++ src/main/java/pt/apollotec/widoes/PinActivity.java
@@ -1,9 +1,7 @@
 package pt.apollotec.widoes;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.annotation.SuppressLint;
-import android.content.*;
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
@@ -17,6 +15,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -29,6 +28,14 @@ import pt.apollotec.R;
 
 
 public class PinActivity extends AppCompatActivity {
+    private static final String SQL_CREATE_ENTRIES =
+            "CREATE TABLE " + FeedEntry.TABLE_NAME + " (" +
+                    FeedEntry._ID + " INTEGER PRIMARY KEY," +
+                    FeedEntry.COLUMN_NAME_1 + " TEXT," +
+                    FeedEntry.COLUMN_NAME_2 + " TEXT," +
+                    FeedEntry.COLUMN_NAME_3 + " TEXT)";
+    private static final String SQL_DELETE_ENTRIES =
+            String.format("DROP TABLE IF EXISTS %s", FeedEntry.TABLE_NAME);
     String url;
     int counterSQLEnteries = 0;
     Button loadbutton;
@@ -37,6 +44,31 @@ public class PinActivity extends AppCompatActivity {
     String Pin;
     String Email;
     String ip = "192.168.1.133";
+
+    public static String md5(final String s) {
+        final String MD5 = "MD5";
+        try {
+            // Create MD5 Hash
+            MessageDigest digest = java.security.MessageDigest
+                    .getInstance(MD5);
+            digest.update(s.getBytes());
+            byte[] messageDigest = digest.digest();
+
+            // Create Hex String
+            StringBuilder hexString = new StringBuilder();
+            for (byte aMessageDigest : messageDigest) {
+                StringBuilder h = new StringBuilder(Integer.toHexString(0xFF & aMessageDigest));
+                while (h.length() < 2)
+                    h.insert(0, "0");
+                hexString.append(h);
+            }
+            return hexString.toString();
+
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,25 +89,27 @@ public class PinActivity extends AppCompatActivity {
                 Pin = EditTextPin.getText().toString();
                 Pin = md5(Pin);
                 Email = EditTextEmail.getText().toString();
-                String urluser = "http://" + ip + "/aponta/GetWebApp.php?email="+Email+"&pin="+Pin;
+                String urluser = "http://" + ip + "/aponta/GetWebApp.php?email=" + Email + "&pin=" + Pin;
                 downloadUrl(urluser);
 
             }
         });
 
         CheckPresentUrl();
-        if (counterSQLEnteries > 0){
+        if (counterSQLEnteries > 0) {
 
             AbrirAgenda(url);
         }
     }
-    void AbrirAgenda(String url){
+
+    void AbrirAgenda(String url) {
         Intent intent = new Intent(this, MainActivity.class);
         intent.putExtra("LinkAgenda", url);
 
         startActivity(intent);
         finish();
     }
+
     private void downloadUrl(final String urlWebService) {
 
         @SuppressLint("StaticFieldLeak")
@@ -89,8 +123,8 @@ public class PinActivity extends AppCompatActivity {
             @Override
             protected void onPostExecute(String s) {
                 super.onPostExecute(s);
-                if (!s.contains("erro")){
-                    AddSQLite(Email,Pin,s);
+                if (!s.contains("erro")) {
+                    AddSQLite(Email, Pin, s);
                     AbrirAgenda(s);
                 }
 
@@ -117,6 +151,42 @@ public class PinActivity extends AppCompatActivity {
         getJSON.execute();
     }
 
+    void CheckPresentUrl() {
+        FeedReaderDbHelper dbHelper = new FeedReaderDbHelper(this);
+        // Gets the data repository in write mode
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+
+        Cursor c = db.rawQuery("SELECT url FROM loadagenda ", null);
+
+        if (c.moveToFirst()) {
+            do {
+                // Passing values
+                url = c.getString(0);
+                // Do something Here with values
+                counterSQLEnteries++;
+            } while (c.moveToNext());
+        }
+        c.close();
+        db.close();
+
+    }
+
+    void AddSQLite(String email, String pin, String url) {
+        FeedReaderDbHelper dbHelper = new FeedReaderDbHelper(this);
+        // Gets the data repository in write mode
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+        // Create a new map of values, where column names are the keys
+        ContentValues values = new ContentValues();
+        values.put(FeedEntry.COLUMN_NAME_1, email);
+        values.put(FeedEntry.COLUMN_NAME_2, pin);
+        values.put(FeedEntry.COLUMN_NAME_3, url);
+
+
+        // Insert the new row, returning the primary key value of the new row
+        db.insert(FeedEntry.TABLE_NAME, null, values);
+        db.close();
+    }
+
     // SQL LITE
     /* Inner class that defines the table contents */
     public static class FeedEntry implements BaseColumns {
@@ -125,16 +195,6 @@ public class PinActivity extends AppCompatActivity {
         static final String COLUMN_NAME_2 = "pin";
         static final String COLUMN_NAME_3 = "url";
     }
-    private static final String SQL_CREATE_ENTRIES =
-            "CREATE TABLE " + FeedEntry.TABLE_NAME + " (" +
-                    FeedEntry._ID + " INTEGER PRIMARY KEY," +
-                    FeedEntry.COLUMN_NAME_1 + " TEXT," +
-                    FeedEntry.COLUMN_NAME_2 + " TEXT," +
-                    FeedEntry.COLUMN_NAME_3 + " TEXT)";
-
-    private static final String SQL_DELETE_ENTRIES =
-            String.format("DROP TABLE IF EXISTS %s", FeedEntry.TABLE_NAME);
-
 
     public static class FeedReaderDbHelper extends SQLiteOpenHelper {
         // If you change the database schema, you must increment the database version.
@@ -144,78 +204,21 @@ public class PinActivity extends AppCompatActivity {
         FeedReaderDbHelper(Context context) {
             super(context, DATABASE_NAME, null, DATABASE_VERSION);
         }
+
         public void onCreate(SQLiteDatabase db) {
             db.execSQL(SQL_CREATE_ENTRIES);
         }
+
         public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
             // This database is only a cache for online data, so its upgrade policy is
             // to simply to discard the data and start over
             db.execSQL(SQL_DELETE_ENTRIES);
             onCreate(db);
         }
+
         public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
             onUpgrade(db, oldVersion, newVersion);
         }
-    }
-
-    void CheckPresentUrl(){
-        FeedReaderDbHelper dbHelper = new FeedReaderDbHelper(this);
-        // Gets the data repository in write mode
-        SQLiteDatabase db = dbHelper.getWritableDatabase();
-
-        Cursor c = db.rawQuery("SELECT url FROM loadagenda ", null);
-
-        if (c.moveToFirst()){
-            do {
-                // Passing values
-                 url = c.getString(0);
-                // Do something Here with values
-                counterSQLEnteries++;
-            } while(c.moveToNext());
-        }
-        c.close();
-        db.close();
-
-    }
-    void AddSQLite(String email, String pin,String url){
-        FeedReaderDbHelper dbHelper = new FeedReaderDbHelper(this);
-        // Gets the data repository in write mode
-        SQLiteDatabase db = dbHelper.getWritableDatabase();
-        // Create a new map of values, where column names are the keys
-        ContentValues values = new ContentValues();
-        values.put(FeedEntry.COLUMN_NAME_1,email);
-        values.put(FeedEntry.COLUMN_NAME_2,pin);
-        values.put(FeedEntry.COLUMN_NAME_3,url);
-
-
-        // Insert the new row, returning the primary key value of the new row
-        db.insert(FeedEntry.TABLE_NAME, null, values);
-        db.close();
-    }
-
-    public static String md5(final String s) {
-        final String MD5 = "MD5";
-        try {
-            // Create MD5 Hash
-            MessageDigest digest = java.security.MessageDigest
-                    .getInstance(MD5);
-            digest.update(s.getBytes());
-            byte[] messageDigest = digest.digest();
-
-            // Create Hex String
-            StringBuilder hexString = new StringBuilder();
-            for (byte aMessageDigest : messageDigest) {
-                StringBuilder h = new StringBuilder(Integer.toHexString(0xFF & aMessageDigest));
-                while (h.length() < 2)
-                    h.insert(0, "0");
-                hexString.append(h);
-            }
-            return hexString.toString();
-
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        }
-        return "";
     }
 
 
